d5fe24173b67d17335e404dcf27dd9a7f260c823
Gonzalo <consor_92@hotmail.com>
2023-03-15 12:29:26 -0300

    Creando primer proyecto maven

---
+++ src/main/java/Clase1/main.java
@@ -0,0 +1,13 @@
+package Clase1;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		System.out.println( "Mi primer proyecto de Maven" );
+		
+		
+	}
+
+}
