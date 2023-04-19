2824f4ee2a9d4fcf040950a84b8c6b1b43f0a98a
Gonzalo <consor_92@hotmail.com>
2023-04-19 12:05:28 -0300

    Collection

--- FlujoDeDatos/src/main/java/Readers/Colecciones.java
+++ FlujoDeDatos/src/main/java/Readers/Colecciones.java
@@ -2,11 +2,17 @@ package Readers;
 
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class Colecciones {
 
@@ -82,7 +88,42 @@ public class Colecciones {
         	String value = InglesEspañol.get( key );
         }
         
+        //ITERATION por Entry
+        //   Entry -> <k,v>
+        for(   Map.Entry<String, String> item : InglesEspañol.entrySet()   )
+        {
+        	item.getKey();
+        	item.getValue();
+        }
         
+        //metodos del HASH MAP
+        InglesEspañol.containsKey("Hello");   //una sola aparicion
+        InglesEspañol.containsValue("Hola");  //varias apariciones
+        InglesEspañol.replace("hi","chau");  //no cambia el KEY   cambia el Value
+        
+        //TREEMAP
+        TreeMap<Integer, String> DNI = new TreeMap<Integer, String>();
+        DNI.put(100000, "pepe");
+        DNI.put(800, "lalo");
+        
+        //LINKEDHASHMAP
+        LinkedHashMap<Integer, Integer> alumnos = new LinkedHashMap<Integer, Integer>();
+        
+        //HASHSET
+        HashSet<Integer> conjuntoNumeros = new HashSet<Integer>();
+        Set<Integer>     conjuntos2      = new HashSet<Integer>();
+        
+        conjuntoNumeros.add( 5 );
+        conjuntoNumeros.contains( 5 );
+        
+        
+        //conversion de Collection
+        //MAP -> keyset
+        Set<String> key = InglesEspañol.keySet();
+        ArrayList<String> value = (ArrayList<String>)InglesEspañol.values();
+  
+        
+        //ESTO PARA DESPUES:   Comparator vs Comparable
         
 	}
 	
