bf600188db35cef74a40328b69b52a6650f4dab1
Gonzalo <consor_92@hotmail.com>
2023-08-30 12:18:30 -0300

    fix

--- ProgramacionSobreRedes/ProgramacionSobreRedes/src/main/java/dao/GeneralDAO.java
+++ ProgramacionSobreRedes/ProgramacionSobreRedes/src/main/java/dao/GeneralDAO.java
@@ -4,9 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.LinkedList;
-import java.util.logging.Logger;
-
-import dto.empleadoDTO;
 
 public interface GeneralDAO<T> {
 	
