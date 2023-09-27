c5fb023c3403a2387f417c7e43804ce660feb4cf
Gonzalo <consor_92@hotmail.com>
2023-09-27 11:32:37 -0300

    aa

--- ProgramacionSobreRedes/ProgramacionSobreRedes/ProgramacionSobreRedes/src/main/java/SocketTCP/servidor.java
+++ ProgramacionSobreRedes/ProgramacionSobreRedes/ProgramacionSobreRedes/src/main/java/SocketTCP/servidor.java
@@ -1,13 +1,26 @@
 package SocketTCP;
 
+import java.io.IOException;
+
 public class servidor extends conexion {
 
-	public servidor(String tipo) {
-		super(tipo);
+	public servidor() {
+		super("servidor");
 	}
 
 	public void serverOn()
 	{
+		try {
+			ps.println("Esperando a un cliente ...");
+			
+			//congela programa
+			sock = servSock.accept();
+			
+			ps.println( sock.getInetAddress().getHostAddress() );
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
