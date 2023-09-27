5b184ab2595d732a6387047a73609ce4764e7d53
Gonzalo <consor_92@hotmail.com>
2023-09-27 12:04:36 -0300

    a

---
+++ ProgramacionSobreRedes/ProgramacionSobreRedes/ProgramacionSobreRedes/src/main/java/SocketTCP/mainServer.java
@@ -0,0 +1,12 @@
+package SocketTCP;
+
+public class mainServer {
+
+	public static void main(String[] args) {
+		
+		servidor s = new servidor();
+		
+		s.serverOn();
+	}
+
+}
--- ProgramacionSobreRedes/ProgramacionSobreRedes/ProgramacionSobreRedes/src/main/java/SocketTCP/servidor.java
+++ ProgramacionSobreRedes/ProgramacionSobreRedes/ProgramacionSobreRedes/src/main/java/SocketTCP/servidor.java
@@ -1,9 +1,15 @@
 package SocketTCP;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class servidor extends conexion {
 
+	InputStreamReader isrServ = null;
+	BufferedReader read = null;
+	
 	public servidor() {
 		super("servidor");
 	}
@@ -16,10 +22,40 @@ public class servidor extends conexion {
 			//congela programa
 			sock = servSock.accept();
 			
-			ps.println( sock.getInetAddress().getHostAddress() );
+			ps.println( "IP: " + sock.getInetAddress().getHostAddress() +
+						"Nombre: " + sock.getInetAddress().getHostName() +
+						"Port: " + sock.getPort() + "\n"
+					  );
+			
+			dosSer = new DataOutputStream( sock.getOutputStream() );
+			isrServ = new InputStreamReader( sock.getInputStream() );
+			read = new BufferedReader( isrServ );
+			
+			ps.println("Cliente conectado con exito :) !!!");
+			
+			ps.println("Esperando mensaje del cliente ...");
+			
+			while(  (msg = read.readLine()) != null )
+			{
+				//aca recibi un mensaje seguro
+				ps.println( msg );
+				
+				dosSer.writeUTF("ok");
+				dosSer.flush();
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
+		}finally {
+			try {
+				sock.close();
+				read.close();
+				isrServ.close();
+				dosSer.close();	
+				servSock.close();				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		
 	}
